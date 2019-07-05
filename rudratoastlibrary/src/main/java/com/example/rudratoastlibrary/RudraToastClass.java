08d2ccd4c221dcf5e4f1c95695bf965efd0e5540
abhishek2221 <abhishek@klizos.com>
2019-07-05 18:18:22 +0530

    Set up new Dialog design and popup window and redirect to ssid.

--- rudratoastlibrary/src/main/java/com/example/rudratoastlibrary/RudraToastClass.java
+++ rudratoastlibrary/src/main/java/com/example/rudratoastlibrary/RudraToastClass.java
@@ -3,9 +3,15 @@ package com.example.rudratoastlibrary;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,57 +25,96 @@ import android.widget.Toast;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class RudraToastClass {
+    static Dialog myDialog;
 
     public interface OnDialogClickListener {
         void onDialogImageRunClick(String success);
     }
     static OnDialogClickListener listener;
-    static PopupWindow mpopup;
-    public static void s(final Context context, String message,OnDialogClickListener clickListener){
 
+    public static void s(final Context context, String message,OnDialogClickListener clickListener){
      listener=clickListener;
 
-        // Create custom dialog object
-        final Dialog dialog = new Dialog(context);
-        // Include dialog.xml file
-        dialog.setContentView(R.layout.dialog);
 
-        dialog.show();
 
-        Button declineButton = (Button) dialog.findViewById(R.id.button);
-        // if decline button is clicked, close the custom dialog
-        declineButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                listener.onDialogImageRunClick("Hogaya re Baba");
-                // Close dialog
-                dialog.dismiss();
+        abhis(context);
 
-            }
-        });
+//        // Create custom dialog object
+//        final Dialog dialog = new Dialog(context);
+//        // Include dialog.xml file
+//        dialog.setContentView(R.layout.dialog);
+//
+//        dialog.show();
+//
+//        Button declineButton = (Button) dialog.findViewById(R.id.button);
+//        // if decline button is clicked, close the custom dialog
+//        declineButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                listener.onDialogImageRunClick("Hogaya re Baba");
+//                // Close dialog
+//                dialog.dismiss();
+//
+//            }
+//        });
+
+
+//        myDialog = new Dialog(context);
+//        myDialog.setContentView(R.layout.dialog);
+//        Button declineButton = (Button) myDialog.findViewById(R.id.button);
+//        declineButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                listener.onDialogImageRunClick("Hogaya re Baba");
+//
+//                Intent intent = new Intent(Intent.ACTION_MAIN);
+//                intent.setComponent(new ComponentName("com.admin.ssid","com.admin.ssid.ui.login"));
+//                context.startActivity(intent);
+//            }
+//        });
+//        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//        myDialog.show();
     }
 
-    public static void getPopUpForUserDetail(final Activity context, String message, OnDialogClickListener clickListener){
-        listener=clickListener;
-        View popUpView = context.getLayoutInflater().inflate(R.layout.view_popup_layout,
-                null); // inflating popup layout
-        mpopup = new PopupWindow(popUpView, ConstraintLayout.LayoutParams.MATCH_CONSTRAINT,
-                ConstraintLayout.LayoutParams.MATCH_CONSTRAINT, true); // Creation of popup
-        mpopup.setAnimationStyle(android.R.style.Animation_Dialog);
-        mpopup.showAtLocation(popUpView, Gravity.CENTER, 0, 0);
 
-        Button btnCancel = (Button) popUpView.findViewById(R.id.button);
-        btnCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    public static void abhis(Context context)
+    {
 
-                listener.onDialogImageRunClick("Hogaya re Baba");
+        String appName = "SSID";
+        String packageName = "com.admin.ssid";
+        openApp(context, appName, packageName);
+    }
+
+    public static void openApp(Context context, String appName, String packageName) {
+        if (isAppInstalled(context, packageName))
+            if (isAppEnabled(context, packageName))
+                context.startActivity(context.getPackageManager().getLaunchIntentForPackage(packageName));
+            else Toast.makeText(context, appName + " app is not enabled.", Toast.LENGTH_SHORT).show();
+        else Toast.makeText(context, appName + " app is not installed.", Toast.LENGTH_SHORT).show();
+    }
+
+
+    private static boolean isAppInstalled(Context context, String packageName) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException ignored) {
+        }
+        return false;
+    }
+
+    private static boolean isAppEnabled(Context context, String packageName) {
+        boolean appStatus = false;
+        try {
+            ApplicationInfo ai = context.getPackageManager().getApplicationInfo(packageName, 0);
+            if (ai != null) {
+                appStatus = ai.enabled;
             }
-        });
-
-
-
-
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return appStatus;
     }
 
 }
