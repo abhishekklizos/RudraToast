14330c8d3cfeb6c6c8e9e04b723901391cc57bcc
abhishek2221 <abhishek@klizos.com>
2019-07-05 11:07:24 +0530

    Added One Dialog

--- rudratoastlibrary/src/main/java/com/example/rudratoastlibrary/RudraToastClass.java
+++ rudratoastlibrary/src/main/java/com/example/rudratoastlibrary/RudraToastClass.java
@@ -1,13 +1,45 @@
 package com.example.rudratoastlibrary;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.widget.Toast;
 
 public class RudraToastClass {
 
-    public static void s(Context c, String message){
 
-        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    public static void s(final Context context, String message){
+
+//        Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+
+        AlertDialog.Builder builder = null;
+        //Uncomment the below code to Set the message and title from the strings.xml file
+        builder.setMessage("Login to SSID") .setTitle("SSID");
+
+        //Setting message manually and performing action on button click
+        builder.setMessage("Do you want to close this application ?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        Toast.makeText(context,"you choose yes action for alertbox",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //  Action for 'NO' Button
+                        dialog.cancel();
+                        Toast.makeText(context,"you choose no action for alertbox",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+        //Creating dialog box
+        AlertDialog alert = builder.create();
+        //Setting the title manually
+        alert.setTitle("AlertDialogExample");
+        alert.show();
 
     }
 
