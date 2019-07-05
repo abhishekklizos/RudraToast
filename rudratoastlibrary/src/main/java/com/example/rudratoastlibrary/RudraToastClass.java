09fd4f149305d9d9ed68f74091eedc93977a225d
abhishek2221 <abhishek@klizos.com>
2019-07-05 15:54:48 +0530

    Set up new Dialog design and popup window.

--- rudratoastlibrary/src/main/java/com/example/rudratoastlibrary/RudraToastClass.java
+++ rudratoastlibrary/src/main/java/com/example/rudratoastlibrary/RudraToastClass.java
@@ -1,14 +1,22 @@
 package com.example.rudratoastlibrary;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.drawable.BitmapDrawable;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class RudraToastClass {
 
@@ -16,6 +24,7 @@ public class RudraToastClass {
         void onDialogImageRunClick(String success);
     }
     static OnDialogClickListener listener;
+    static PopupWindow mpopup;
     public static void s(final Context context, String message,OnDialogClickListener clickListener){
 
      listener=clickListener;
@@ -38,6 +47,29 @@ public class RudraToastClass {
 
             }
         });
+    }
+
+    public static void getPopUpForUserDetail(final Activity context, String message, OnDialogClickListener clickListener){
+        listener=clickListener;
+        View popUpView = context.getLayoutInflater().inflate(R.layout.view_popup_layout,
+                null); // inflating popup layout
+        mpopup = new PopupWindow(popUpView, ConstraintLayout.LayoutParams.MATCH_CONSTRAINT,
+                ConstraintLayout.LayoutParams.MATCH_CONSTRAINT, true); // Creation of popup
+        mpopup.setAnimationStyle(android.R.style.Animation_Dialog);
+        mpopup.showAtLocation(popUpView, Gravity.CENTER, 0, 0);
+
+        Button btnCancel = (Button) popUpView.findViewById(R.id.button);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                listener.onDialogImageRunClick("Hogaya re Baba");
+            }
+        });
+
+
+
+
     }
 
 }
