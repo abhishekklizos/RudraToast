07f9b36b248e881e224984e2ea47715e4dfe68d3
abhishek2221 <abhishek@klizos.com>
2019-07-05 13:31:25 +0530

    Set up with Dialog listner

--- rudratoastlibrary/src/main/java/com/example/rudratoastlibrary/RudraToastClass.java
+++ rudratoastlibrary/src/main/java/com/example/rudratoastlibrary/RudraToastClass.java
@@ -1,15 +1,24 @@
 package com.example.rudratoastlibrary;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class RudraToastClass {
 
+    public interface OnDialogClickListener {
+        void onDialogImageRunClick(String success);
+    }
+    static OnDialogClickListener listener;
+    public static void s(final Context context, String message,OnDialogClickListener clickListener){
 
-
-    public static void s(final Context context, String message){
+     listener=clickListener;
 
 //        Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
 
@@ -24,6 +33,8 @@ public class RudraToastClass {
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
 
+                        listener.onDialogImageRunClick("Hogaya re Baba");
+
                         Toast.makeText(context,"you choose yes action for alertbox",
                                 Toast.LENGTH_SHORT).show();
                     }
@@ -31,7 +42,9 @@ public class RudraToastClass {
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         //  Action for 'NO' Button
+                        listener.onDialogImageRunClick("Hogaya re Baba,Chaap kyo leta hai");
                         dialog.cancel();
+
                         Toast.makeText(context,"you choose no action for alertbox",
                                 Toast.LENGTH_SHORT).show();
                     }
@@ -40,9 +53,27 @@ public class RudraToastClass {
         //Creating dialog box
         AlertDialog alert = builder.create();
         //Setting the title manually
-        alert.setTitle("AlertDialogExample");
+
         alert.show();
 
+
+        // Create custom dialog object
+        final Dialog dialog = new Dialog(context);
+        // Include dialog.xml file
+        dialog.setContentView(R.layout.dialog);
+
+        dialog.show();
+
+        Button declineButton = (Button) dialog.findViewById(R.id.button);
+        // if decline button is clicked, close the custom dialog
+        declineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Close dialog
+                dialog.dismiss();
+
+            }
+        });
     }
 
 }
