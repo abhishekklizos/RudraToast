f4a02f36feae660de19b19b3057aca6e4696f906
abhishek2221 <abhishek@klizos.com>
2019-07-15 19:15:41 +0530

    Finally Done the Level One Work.

---
+++ rudratoastlibrary/src/main/java/com/example/rudratoastlibrary/ExamoActivity.java
@@ -0,0 +1,26 @@
+package com.example.rudratoastlibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class ExamoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_examo);
+
+
+
+    }
+
+    public static void LoginManagerConnect(Activity context, String packg){
+//        listenerDone=clickListener;
+//        context.startActivity(new Intent(context,LoginActivity.class));
+
+    }
+
+}
